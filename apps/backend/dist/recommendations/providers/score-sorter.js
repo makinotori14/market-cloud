c59885a05ce8255f90cb1c384534a06d2b084224
Albert Han <makinotori14@gmail.com>
2026-05-11 11:59:33 +0300

    upd

--- apps/backend/dist/recommendations/providers/score-sorter.js
+++
@@ -1,4 +0,0 @@
-export function sortByFinalScore(recommendations) {
-    return [...recommendations].sort((left, right) => right.finalScore - left.finalScore);
-}
-//# sourceMappingURL=score-sorter.js.map
